8333bfba66318fcee5b1247b4f67cb5ad94e187b
Wasan Leelaloetphanit <wasan.l@tangerine.co.th>
2020-09-29 09:17:36 +0700

    fix import

--- src/main/java/com/demo/api/FirebaseDatabase/controller/FirebaseDatabaseController.java
+++ src/main/java/com/demo/api/FirebaseDatabase/controller/FirebaseDatabaseController.java
@@ -1,23 +1,15 @@
 package com.demo.api.FirebaseDatabase.controller;
 
-import java.util.concurrent.ExecutionException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.demo.api.FirebaseDatabase.entity.User;
-import com.demo.api.FirebaseDatabase.service.RealtimeDatabaseService;
+import com.demo.api.FirebaseDatabase.service.FirebaseDatabaseService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirebaseDatabaseController {
 
     @Autowired
-    RealtimeDatabaseService realtimeDatabaseService;
+    FirebaseDatabaseService firebaseDatabaseService;
 
     @GetMapping(value = "/check")
     public ResponseEntity<String> checkStatus(HttpServletRequest httpServletRequest, HttpServletResponse httpResponse) {
--- src/main/java/com/demo/api/FirebaseDatabase/service/FirebaseDatabaseService.java
+++ src/main/java/com/demo/api/FirebaseDatabase/service/FirebaseDatabaseService.java
@@ -1,5 +1,7 @@
 package com.demo.api.FirebaseDatabase.service;
 
+import org.springframework.stereotype.Service;
+
 @Service
 public class FirebaseDatabaseService {
 
